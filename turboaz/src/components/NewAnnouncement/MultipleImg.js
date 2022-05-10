083b56bfee529dd60d82c24b415059abdff9d0d9
rovshanguli <rovshanshg@code.edu.az>
2022-05-10 17:20:03 +0400

    ResizeUploadArea

--- turboaz/src/components/NewAnnouncement/MultipleImg.js
+++ turboaz/src/components/NewAnnouncement/MultipleImg.js
@@ -1,72 +1,21 @@
 import React, { useState } from "react";
-import FolderIcon from "../../assets/img/newannouncement/uploadimg/frontofcar.png";
+import FrontCar from "../../assets/img/newannouncement/uploadimg/frontofcar.png";
+import BackCar from "../../assets/img/newannouncement/uploadimg/backofcar.png";
 import CloseIcon from "../../assets/img/newannouncement/uploadimg/CloseIcon.svg";
-import styled from 'styled-components'
+import styled from 'styled-components';
+import '../../assets/css/newannouncement/multipleimg.scss'
 
-export const Layout = styled.div`
-    height: 100%;
-    width: 100%;
 
-    display: grid;
-    place-items: center;
 
-    background: #F5F8FF;
-`
 
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    background: #fff;
-    padding: 36px 48px;
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-    border-radius: 20px;
-    text-align: center;
-
-    p {
-        margin-top: -10px;
-        color: #777;
-    }
-`
-
-export const BoxUpload = styled.div`
-    display: grid;
-    margin-top: 20px;
-    place-items: center;
-    border: 1px dashed #799CD9;
-    /* padding: 36px 48px; */
-    position: relative;
-
-    height: 350px;
-    width: 350px;
-
-    background: #FBFBFF;
-    border-radius: 20px;
-
-    .image-upload {
-        display: flex;
-        flex-wrap:wrap;
-
-        label {
-            cursor: pointer;
-        
-            :hover {
-                opacity: .8;
-            }
-        }
-
-        >input {
-            display: none;
-        }
-    }
-`
 
 export const ImagePreview = styled.div`
     position: relative;
     /* cursor: pointer; */
 
     #uploaded-image{
-        height: 350px;
-        width: 350px;
+        height: 100%;
+        width: 100%;
         object-fit: cover;
         border-radius: 20px;
     }
@@ -90,11 +39,14 @@ export const ImagePreview = styled.div`
 
 
 function App() {
+
   const [image, setImage] = useState("");
   const [isUploaded, setIsUploaded] = useState(false);
   const [typeFile, setTypeFile] = useState("");
 
+
   function handleImageChange(e) {
+   
     if (e.target.files && e.target.files[0]) {
       setTypeFile(e.target.files[0].type);
       let reader = new FileReader();
@@ -109,20 +61,24 @@ function App() {
   }
 
   return (
-    <Layout>
-      <Container>
-        <BoxUpload>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-md-6 col-sm-12">
+
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 box-upload">
           <div className="image-upload">
             {!isUploaded ? (
               <>
                 <label htmlFor="upload-input">
                   <img
-                    src={FolderIcon}
+                    src={FrontCar}
                     draggable={"false"}
                     alt="placeholder"
-                    style={{ width: '100%', height: '100%' }}
+                    style={{ width: '100%'}}
                   />
-                  <p style={{ color: "#444" }}>Click to upload image</p>
+                  <p style={{ color: "#444" }} className="m-0">Click to upload image</p>
                 </label>
 
                 <input
@@ -163,12 +119,67 @@ function App() {
               </ImagePreview>
             )}
           </div>
-        </BoxUpload>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 box-upload">
+        <div className="image-upload">
+            {!isUploaded ? (
+              <>
+                <label htmlFor="upload-input">
+                  <img
+                    src={BackCar}
+                    draggable={"false"}
+                    alt="placeholder"
+                    style={{ width: '100%'}}
+                  />
+                  <p style={{ color: "#444" }} className="m-0">Click to upload image</p>
+                </label>
+
+                <input
+                  id="upload-input"
+                  type="file"
+                  accept=".jpg,.jpeg,.gif,.png,.mov,.mp4"
+                  onChange={handleImageChange}
+                />
+              </>
+            ) : (
+              <ImagePreview>
+                <img
+                  className="close-icon"
+                  src={CloseIcon}
+                  alt="CloseIcon"
+                  onClick={() => {
+                    setIsUploaded(false);
+                    setImage(null);
+                  }}
+                />
+                {typeFile.includes("video") ? (
+                  <video
+                    id="uploaded-image"
+                    src={image}
+                    draggable={false}
+                    controls
+                    autoPlay
+                    alt="uploaded-img"
+                  />
+                ) : (
+                  <img
+                    id="uploaded-image"
+                    src={image}
+                    draggable={false}
+                    alt="uploaded-img"
+                  />
+                )}
+              </ImagePreview>
+            )}
+          </div>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12 box-upload">
+        </div>
+      </div>
+    </div>
 
 
 
-      </Container>
-    </Layout>
   );
 }
 
